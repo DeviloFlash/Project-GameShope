aba4dbb2b9f6069ea710fd92a600af10fcb5a28b
DeviloFlash <52181096+DeviloFlash@users.noreply.github.com>
2020-01-28 20:50:04 +0700

    Update promotion_user.php

--- application/views/user/promotion_user.php
+++ application/views/user/promotion_user.php
@@ -15,7 +15,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<div class="hots-bar">
 			<img src="<?php echo base_url(); ?>assist/img/bgsetmain.png">
 				<div class="headinfo" data-aos="zoom-in-left">
-					กกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกก
+					Gameshope
 				</div>
 				<div class="text-bar" data-aos="zoom-in-left">
 					<div>
